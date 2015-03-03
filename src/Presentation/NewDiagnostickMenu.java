96165ba52568308b2f592f1f09445e3fe045e477
Christian Haase <Christian.haase.93@gmail.com>
2015-03-03 10:01:13 +0100

    Menu for creating a new diagnose created - Fix the IllnessData to use Diagnose instead of BEIllness

--- src/Presentation/NewDiagnostickMenu.java
+++ src/Presentation/NewDiagnostickMenu.java
@@ -5,19 +5,49 @@
  */
 package Presentation;
 
+import BLL.IllnessData;
+import Models.Diagnose;
+import java.util.Scanner;
+
 /**
  *
  * @author Niels
  */
 public class NewDiagnostickMenu extends Menu{
 
+    private Scanner sc;
+    
     public NewDiagnostickMenu() {
-        super("Enter name", "Enter diagnose");
+        super("enter diagnose", "Enter new diagnose");
+        sc = new Scanner(System.in);
     }
 
     @Override
     protected void doAction(int option) {
+        switch(option){
+            case 1: 
+                createNewDiagnose();
+                break;
+        }
+    }
+    
+    private void createNewDiagnose(){
+        System.out.println("Enter doctors name: ");
+        String dName = sc.nextLine();
+        System.out.println("Doctors name: " + dName);
+        System.out.println("");
+        System.out.println("Enter patients name: ");
+        String pName = sc.nextLine();
+        System.out.println("Patients name: " + pName);
+        System.out.println("");
+        System.out.println("Etner diagnose: ");
+        String diagnose = sc.nextLine();
         
+        newDiagnose(dName, pName, diagnose);
+    }
+    
+    private void newDiagnose(String dName, String pName, String diagnose){
+        Diagnose diag = new Diagnose(dName, pName, diagnose);
     }
     
 }
