d4d56a853f11e2a0c45d88c1e7733cc1a13d32bd
brobak <brobaks@yahoo.dk>
2015-03-03 09:35:14 +0100

    Added symptoms

--- src/BE/BEIllness.java
+++ src/BE/BEIllness.java
@@ -6,15 +6,24 @@
 
 package BE;
 
+import java.util.ArrayList;
+
 
 public class BEIllness {
     private String name;
     private String description;
+    private ArrayList<BESymptom> symptoms;
     
     
-    public BEIllness(String name, String description){
+    public BEIllness(String name, String description, ArrayList<BESymptom> symptoms){
         this.name = name;
         this.description = description;
+        this.symptoms = symptoms;
+    }
+     public BEIllness(String name, String description){
+        this.name = name;
+        this.description = description;
+        this.symptoms = new ArrayList<BESymptom>();
     }
     
     /**
@@ -33,8 +42,26 @@ public class BEIllness {
         return this.description;
     }
     
+    private ArrayList<BESymptom> getSymptoms(){
+        return symptoms;
+    }
+    
     @Override
     public String toString(){
         return this.name;
+    }
+    
+    private void addSymptom(BESymptom symptom){
+        symptoms.add(symptom);
+    }
+    
+    private void removeSymptom(BESymptom symptom){
+        for (BESymptom symptom1 : symptoms) {
+            if(symptom1.equals(symptom))
+            {
+                symptoms.remove(symptom1);
+                return;
+            }
+        }
     }
 }
