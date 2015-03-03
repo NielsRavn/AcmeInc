b14fdb21ec5502ed08775bddfcc1b344ff31c868
Christian Haase <Christian.haase.93@gmail.com>
2015-03-03 08:34:27 +0100

    Comm 1

--- src/testtictac/TestTictac.java
+++ src/testtictac/TestTictac.java
@@ -9,7 +9,6 @@ package testtictac;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
