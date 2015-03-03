adfa4404f5b0a772c8c65dd39e74ecc9d36134e8
niels <nkr@techraven.dk>
2015-03-03 10:29:58 +0100

    diagnose

--- src/Presentation/NewDiagnostickMenu.java
+++ src/Presentation/NewDiagnostickMenu.java
@@ -5,6 +5,8 @@
  */
 package Presentation;
 
+import BE.BEDiagnose;
+import BLL.DiagnoseData;
 import BLL.IllnessData;
 import java.util.Scanner;
 
@@ -39,14 +41,15 @@ public class NewDiagnostickMenu extends Menu{
         String pName = sc.nextLine();
         System.out.println("Patients name: " + pName);
         System.out.println("");
-        System.out.println("Etner diagnose: ");
+        System.out.println("Enter diagnose: ");
         String diagnose = sc.nextLine();
         
         newDiagnose(dName, pName, diagnose);
     }
     
     private void newDiagnose(String dName, String pName, String diagnose){
-       // Diagnose diag = new Diagnose(dName, pName, diagnose);
+        BEDiagnose diag = new BEDiagnose(dName, pName, diagnose);
+        DiagnoseData.getInstance().addDiagnose(diag);
     }
     
 }
