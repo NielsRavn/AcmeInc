ca776bda00b7470fa5ed55f9cb633fe5ba32fd3a
Zoracka <atzoracka@gmail.com>
2015-03-03 09:40:54 +0100

    ?

--- src/BLL/IllnessData.java
+++ src/BLL/IllnessData.java
@@ -21,11 +21,20 @@ public class IllnessData {
         BESympton symptonBarf = new BESympton("barf");
         BESympton symptonHeadache = new BESympton("headache");
         BESympton symptonStomachAche = new BESympton("stomach ache");
-      
+        BESympton symptonShit = new BESympton("Shits alot");
+        ArrayList<BESympton> symptons = new ArrayList<>();
+        symptons.add(symptonMild);
+        symptons.add(symptonBarf);
         ArrayList<BEIllness> data = new ArrayList<>();
-        data.add(new BEIllness("Flu (Influenza)","The flu is an infectious disease caused by the influenza virus. Symptoms can be mild to severe. The most common symptoms include: a high fever, runny nose, sore throat, muscle pains, headache, coughing, and feeling tired. "));
-        data.add(new BEIllness("MRSA (Methicillin-resistant Staphylococcus aureus)","MRSA is an infection is caused by a strain of staph bacteria that's become resistant to the antibiotics commonly used to treat ordinary staph infections."));
-        data.add(new BEIllness("Gonorrhea","Gonorrhoea is a common sexually transmitted infection caused by the bacterium Neisseria gonorrhoeae."));
+        data.add(new BEIllness("Flu (Influenza)","The flu is an infectious disease caused by the influenza virus. Symptoms can be mild to severe. The most common symptoms include: a high fever, runny nose, sore throat, muscle pains, headache, coughing, and feeling tired. ", symptons));
+        symptons.clear();
+        symptons.add(symptonSevere);
+        symptons.add(symptonHeadache);
+        symptons.add(symptonStomachAche);
+        data.add(new BEIllness("MRSA (Methicillin-resistant Staphylococcus aureus)","MRSA is an infection is caused by a strain of staph bacteria that's become resistant to the antibiotics commonly used to treat ordinary staph infections.", symptons));
+        symptons.clear();
+        symptons.add(symptonShit);
+        data.add(new BEIllness("Gonorrhea","Gonorrhoea is a common sexually transmitted infection caused by the bacterium Neisseria gonorrhoeae.", symptons));
         return data;
     }
 }
